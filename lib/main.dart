9c09e7f3dde4d7d536a140829242a926233426a9
Sayed2580 <alawisayed792@gmail.com>
2022-07-20 12:35:14 +0300

    for loop

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'dart:io';
 
 void main() {
-  Map<String, int> salries = {'Sayed': 50, 'Samy': 100, 'Kareem': 150};
-  
-  // Map<int, String> students = {99: 'Sayed', 1: 'Samy', 2: 'Kareem'};
-
-  // print(students[99]);
-
-  print(salries);
+  for (int i = 0; i < 5; i = i + 1) {
+    print('Sayed');
+  }
 }
