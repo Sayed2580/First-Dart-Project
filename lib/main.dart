3902b0f8e9dda6cb5f085a7f254ad54e911cda07
! Sayed <alawisayed792@gmail.com>
2022-09-09 10:27:23 +0300

    arrow function

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'dart:io';
 
 void main() {
-  printData(firstName: 'Sayed' , secondName: 'Mohammed' , age: 25);
+  displayName();
 }
 
-void printData({String? firstName, String? secondName, int? age = 25}) {
-  print('first name is $firstName');
-  print('second name is $secondName');
-  print('age is $age');
-}
+String displayName() => 'Sayed';
