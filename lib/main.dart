48628353e8d1e8b51d2f01da8924c39f758beff3
! Sayed <alawisayed792@gmail.com>
2022-09-10 10:16:07 +0300

    object and classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'dart:io';
 
 void main() {
+  Human Sayed = Human();
+  Sayed.hairColor = 'black';
+  Sayed.hight = 150;
+  Sayed.weight = 50;
 
+  Human Ali = Human();
+  Ali.hight = 170;
+  Ali.weight = 70;
+  Ali.hairColor = 'black';
+
+  print(Sayed.hight);
+  print(Ali.hight);
 }
 
 
-int getNumber() {
-  int x;
-  int y = 50;
-
-
-  return y ==0 ? 100 : -100;
+class Human {
+  int numberOfArms = 2;
+  double? hight;
+  double? weight;
+  String? hairColor;
 }
